cf6010f69275944a4737e160af66245f4747384f
zhan-hc <zhan19527@gmail.com>
2020-03-09 10:16:52 +0800

    freemall finish

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">首页</a>
+        <span>{{crumbs}}</span>
+      </nav>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+    name: 'nav-crumbs',
+    props:['crumbs']
+}
+</script>
